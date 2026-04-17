74e006f68d0664509035ea4f102f7990240fbde6
iamCoderMohit <mohitjoshiu@gmail.com>
2026-04-17 19:49:42 +0530

    for loops

---
+++ loops/for.py
@@ -0,0 +1,58 @@
+# while -> we dont know when to end the loop
+
+
+# for loop -> we know when to end the loop
+
+name = "mahima" #5, len -> 6
+# index
+
+
+# i = 0
+# while i < len(name):
+#     print(name[i])
+#     i += 1
+
+# for i in name: #0, 1, 2, 3....
+#     print(i)
+
+# range(start, end)
+# range(end)
+
+# for i in range(10, 20): #0, 10
+#     print(i)
+
+# 2 -> 2 4 6 8 10....
+
+# i = 1
+# n = int(input("enter : "))
+
+
+# while i <= 10:
+#     print(n*i)
+#     i += 1
+
+# range(start, end, step)
+# i = 0
+# while i < 11:
+#     print(i)
+#     i += 2
+
+# for i in range(1, 11):
+#     print()
+
+# for i in range(1, 11, 3):
+#     print(i)
+
+# enumeration()
+
+# iterable -> those data types where you can use loops
+
+name = "python"
+# p-0, y-1, t-2..... 
+# n = 10
+
+#0 - p
+#1 - y
+
+for i, v in enumerate(name):
+    print(i, v)
---
+++ loops/nested.py
@@ -0,0 +1,39 @@
+# nested -> 
+
+# for i in range(5):
+#     for j in range(3):
+#         for k in range(2):
+#             print(k)
+
+#0
+#   0
+#       0
+#       1
+#   1
+#       0
+#       1
+#   2
+#       0
+#       1
+
+#1
+#   0
+#   1
+#   2
+
+#2
+#   0
+#   1
+#   2
+
+#3
+
+#4
+
+for i in range(10):
+    if(i == 50):
+        break
+
+    print(i)
+else:
+    print("done")
